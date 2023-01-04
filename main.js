e5f8fe253ccdf81f2d28aa0ca832954c2e79569d
Asphy <arodriguezvera1997@gmail.com>
2023-01-03 21:31:42 -0300

    se agrega dom, eventos con addeventlistener y storage&json para guardar los datos escritos en la pestaña de contactanos. Se agregaron MUCHOS cambios a la pag original y pasó a tener un diseño mas minimalista. Se corregirá lo que el tutor indique

--- main.js
+++ main.js
@@ -16,6 +16,15 @@ function increaseQuantity( button ) {
   parent.querySelector("#item-count").innerHTML = count;
 }
 
+if (document.querySelector(".toggle-dark-mode") != null) {
+  const toggle = document.querySelector('.toggle-dark-mode');
+    toggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+  });
+} else {
+
+}
+
 window.onload = function() {
   var cart = JSON.parse(localStorage.getItem("cart"));
   if (cart) {
@@ -25,42 +34,66 @@ window.onload = function() {
   }
 };
 
-function addToCart() {
+const clickFormulario = document.querySelector(".botonClickeado")
+clickFormulario.addEventListener("click", submitForm, true)
+
+function submitForm( event ) {
+  // event.preventDefault()
   var nombre = document.getElementById("nombre").value;
-  var valor = document.getElementById("valor").value;
-  var cantidad = document.getElementById("cantidad").value;
-  
+  var numero = document.getElementById("numeroTelefono").value;
+  var correo = document.getElementById("correo").value;
+  var comentarios = document.getElementById("comentarios").value;
+  const cambioJSON = {
+    "nombre": nombre,
+    "numero": numero,
+    "correo": correo,
+    "comentarios": comentarios,
+    }
+  document.querySelector(".result").innerHTML = "Has enviado tus datos. ¡En breve te responderemos!"
+  localStorage.setItem('datosDeContacto', JSON.stringify(cambioJSON))
+  document.querySelector(".claseFormulario").reset();
+  console.log(localStorage.getItem("datosDeContacto"))
+} 
+
+
+
+// const addItem = document.querySelector(".carroCompras")
+
+// addItem.onclick = ( event ) => {
+
+// }
+
+function addToCart() {
+  var name = document.getElementById("name");
+  var price = document.getElementById("price");
+  var quantity = document.getElementById("quantity");
+
   var item = {
-    nombre: nombre,
-    valor: valor,
-    cantidad: cantidad,
-};
-  
-addItemToCart(item);
+    name: name,
+    price: price,
+    quantity: quantity,
+  };
+
+  addItemToCart(item);
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 function addItemToCart(item) {
   var itemElement = document.createElement("div");
   itemElement.className = "item";
-  
+
   var nameElement = document.createElement("p");
   nameElement.innerHTML = item.name;
   itemElement.appendChild(nameElement);
-  
+
   var priceElement = document.createElement("p");
   priceElement.innerHTML = "$" + item.price;
   itemElement.appendChild(priceElement);
-  
+
   var quantityElement = document.createElement("p");
-  quantityElement.innerHTML = item.quantity + " unidades";
+  quantityElement.innerHTML = item.quantity + "unidades";
   itemElement.appendChild(quantityElement);
-  
+
   var cartElement = document.getElementById("cart");
   cartElement.appendChild(itemElement);
 }
-
-const toggle = document.querySelector('.activar-modo-oscuro');
-    toggle.addEventListener('click', () => {
-    document.body.classList.toggle('modo-oscuro');
-});
